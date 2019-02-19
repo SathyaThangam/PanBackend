244126f0fb75e8071f424272ff7ca08f73437e95
chittranjan@1997 <chittranjan@codingmart.com>
2019-02-19 19:52:02 +0530

    config file changed

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   apps : [{
-    name: 'PancardBackend',
+    name: 'PanBackend',
     script: 'npm',
 
     // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
@@ -22,10 +22,10 @@ module.exports = {
       user : 'root',
       host : '159.89.165.174',
       ref  : 'origin/master', 
-      repo : 'https://github.com/SathyaThangam/PancardBackend.git',
-      path : '/var/www/PancardBackend',
+      repo : 'https://github.com/SathyaThangam/PanBackend.git',
+      path : '/var/www/PanBackend',
       // 'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
-      'post-deploy' : 'npm install --production && pm2 del PancardBackend || npm run production'
+      'post-deploy' : 'npm install --production && pm2 del PanBackend || npm run production'
       
     }
   }
